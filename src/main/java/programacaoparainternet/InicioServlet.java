6c05384c6e82476384da75770064eead2eedb733
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2017-08-21 19:53:40 -0300

    Primeiro commit

---
+++ src/main/java/programacaoparainternet/InicioServlet.java
@@ -0,0 +1,9 @@
+package programacaoparainternet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(urlPatterns = {""})
+public class InicioServlet extends HttpServlet{
+
+	doget
+}
